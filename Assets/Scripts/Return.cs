43809dcb4a5ef8db1878af07f34f52b826a63430
Nick Olenz <nickolenz@comcast.net>
2015-12-10 13:32:27 -0600

    Added return script. Press 'r'

---
+++ Assets/Scripts/Return.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Return : MonoBehaviour
+{
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			Application.LoadLevel("Lobby");
+	}
+}
