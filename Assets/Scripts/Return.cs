f26b6b7dcb9a93afda068f72c7db3513ce743b1a
unknown <olenz2@SIEBL-4240-06.ad.uillinois.edu>
2015-12-10 15:45:08 -0600

    Arrowkey turning.

--- Assets/Scripts/Return.cs
+++ Assets/Scripts/Return.cs
@@ -7,5 +7,12 @@ public class Return : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.R))
 			Application.LoadLevel("Lobby");
+
+
+		// Test turning
+		if (Input.GetKeyDown (KeyCode.LeftArrow))
+			transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y - 20, transform.localEulerAngles.z);
+		if (Input.GetKeyDown (KeyCode.RightArrow))
+			transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y + 20, transform.localEulerAngles.z);
 	}
 }
