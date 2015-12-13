9637b0cb7b1ee0ee6981fa380310dcdc6d89ceec
Nick Olenz <nickolenz@comcast.net>
2015-12-13 02:22:11 -0600

    Scene changes. Discrete movement on arrowkeys.

--- Assets/Scripts/Return.cs
+++ Assets/Scripts/Return.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Return : MonoBehaviour
 {
+	// This script has become a bit of a potpurri of madness. It all works though.
+	// Kinda.
+
 	void Update ()
 	{
 		if (Input.GetKeyDown(KeyCode.R))
@@ -14,6 +17,29 @@ public class Return : MonoBehaviour
 			transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y - 30, transform.localEulerAngles.z);
 		if (Input.GetKeyDown (KeyCode.RightArrow))
 			transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y + 30, transform.localEulerAngles.z);
+
+		// Dr. Yershova asked for discrete forward and backwards movement, so here's that. Need some proactive checks so you can't fall through the world.
+		// It doesn't work very well. Also, carpal tunnel.
+		// Forward / Backward
+		LayerMask level = 1 << LayerMask.NameToLayer ("Default");
+		Vector3 move = transform.forward;
+		move.y = 2;
+		bool hit = Physics.Raycast (transform.position + (0.25f * transform.forward), transform.position + (2f * move), level);
+		bool backHit = Physics.Raycast (transform.position - (0.25f * transform.forward), transform.position - (2f * move), level);
+		if (Input.GetKeyDown (KeyCode.UpArrow) && !hit) 
+		{
+			transform.position = transform.position + transform.forward;
+			transform.position = new Vector3(transform.position.x, transform.position.y + 0.4f, transform.position.z);
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow) && !backHit) 
+		{
+			transform.position = transform.position - transform.forward;
+		}
+
+		// Just-In-Case position reset
+		if (transform.position.y < -20)
+			Application.LoadLevel (Application.loadedLevel);
 
 		// 360 Controller code
 		/*
